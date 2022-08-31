68598cb4e1ec4d7b55832b545adbfb9b402c5357
shahariarr <99550336+shahariarr@users.noreply.github.com>
2022-08-31 21:56:25 +0600

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    double x=3905,y=9000,sub,avg;
+     sub=y-x;
+     avg=(sub/4);
+     printf("Amount of money per person: %lf\n",avg);
+    printf("Enter your roll number: ");
+  int s,l,r,t,roll;
+    scanf("%d",roll);
+        if(roll==6024)
+        {
+          printf("Your amount of money %d",avg);
+        }
+    return 0;
+}
+
